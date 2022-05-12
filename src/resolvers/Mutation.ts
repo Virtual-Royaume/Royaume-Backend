a1e4733042621ce6c809b07164fd61d1f98e13dd
Bluzzi <camilledugas.pro@gmail.com>
2022-05-13 00:58:43 +0200

    Mutation: add members mutations

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,10 +1,12 @@
 import roleCollection from "../database/collections/MainRole.js";
 import memberCollection, { createMember } from "../database/collections/Member.js";
+import serverActivityCollection, { getServerActivity } from "../database/collections/ServerActivity.js";
 import { Resolvers } from "../interfaces/GraphQL.js";
+import { getDateWithoutTime } from "../utils/Date.js";
 
 const mutation: Resolvers["Mutation"] = {
-	createMember: async (_, { id, username, profilPicture, isOnServer }) => 
-		await createMember(id, username, profilPicture, isOnServer ?? true),
+  // MEMBER :
+	createMember: async (_, { id, username, profilPicture, isOnServer }) => await createMember(id, username, profilPicture, isOnServer ?? true),
 
   updateMember: async (_, { id, input }) => {
     // Remove null properties of input :
@@ -22,9 +24,43 @@ const mutation: Resolvers["Mutation"] = {
     }
   },
 
-  updateMemberDiscordActivity: () => false, // TODO
+  updateMemberDiscordActivity: async (_, { id, input }) => {
+    const newData: { [key: string]: any } = {}
+
+    if(input.messageMonthCount) newData["activity.messages.monthCount"] = input.messageMonthCount;
+    if(input.messageTotalCount) newData["activity.messages.totalCount"] = input.messageTotalCount;
+    if(input.voiceMinute) newData["activity.voiceMinute"] = input.voiceMinute;
+    
+    try {
+      await memberCollection.updateOne({ _id: id }, { $set: newData });
+
+      return true;
+    } catch {
+      return false;
+    }
+  },
   
-  updateMemberDiscordActivityChannel: () => false // TODO
+  updateMemberDiscordActivityChannel: async (_, { id, channelId, messageCount }) => {
+    try {
+      // Try to update a existing element :
+      const result = await memberCollection.updateOne(
+        { _id: id, "activity.messages.perChannel.channelId": channelId }, 
+        { $set: { "activity.messages.perChannel.$.messageCount": messageCount } }
+      );
+        
+      // If element does not exist, push a new element :
+      if(!result.modifiedCount){
+        await memberCollection.updateOne(
+          { _id: id }, { $push: { "activity.messages.perChannel": { channelId, messageCount } } }
+        );
+      }
+
+      return true;
+    } catch {
+      return false;
+    }
+  },
+
   // ROLES :
   addRole: async (_, { roleId, category }) => 
     (await roleCollection.updateOne({ roleId }, { $setOnInsert: { roleId, category } }, { upsert: true })).acknowledged, // function return tjr true
