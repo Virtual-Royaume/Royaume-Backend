3f334fc1f015f16db3cf52016866ae9048b6dd39
Bluzzi <camilledugas.pro@gmail.com>
2022-05-13 19:03:02 +0200

    Move Date Scalar in a specific file

--- src/resolvers/Resolver.ts
+++ src/resolvers/Resolver.ts
@@ -1,24 +1,10 @@
-import { GraphQLScalarType, Kind } from "graphql";
 import { Resolvers } from "../interfaces/GraphQL.js";
-import { getDateWithoutTime } from "../utils/Date.js";
 import Mutation from "./Mutation.js";
 import Query from "./Query.js";
+import { date } from "./Scalar.js";
 
 const resolvers: Resolvers = {
-  Date: new GraphQLScalarType({
-    name: "Date",
-    description: "Date without time",
-
-    serialize(value){
-      return getDateWithoutTime((value as Date)).getTime();
-    },
-    parseValue(value){
-      return getDateWithoutTime(new Date(value as string));
-    },
-    parseLiteral(ast){
-      return ast.kind === Kind.STRING ? getDateWithoutTime(new Date(ast.value)) : null;
-    }
-  }),
+  Date: date,
   Query, Mutation
 }
 
---
+++ src/resolvers/Scalar.ts
@@ -0,0 +1,18 @@
+import { GraphQLScalarType, Kind } from "graphql";
+import { Resolvers } from "../interfaces/GraphQL.js";
+import { getDateWithoutTime } from "../utils/Date.js";
+
+export const date: Resolvers["Date"] = new GraphQLScalarType({
+  name: "Date",
+  description: "Date without time",
+
+  serialize(value){
+    return getDateWithoutTime((value as Date)).getTime();
+  },
+  parseValue(value){
+    return getDateWithoutTime(new Date(value as string));
+  },
+  parseLiteral(ast){
+    return ast.kind === Kind.STRING ? getDateWithoutTime(new Date(ast.value)) : null;
+  }
+});
