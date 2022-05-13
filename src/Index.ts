dc9f95bfa85e2a47b97b62b41de736d54f204c5a
Bluzzi <camilledugas.pro@gmail.com>
2022-05-13 19:04:39 +0200

    Index: fix tasks dynamics import

--- src/Index.ts
+++ src/Index.ts
@@ -6,7 +6,7 @@ import path from "path";
 import resolvers from "./resolvers/Resolver.js";
 
 // Load tasks :
-readdirSync(path.resolve() + "/src/tasks").forEach(file => import(`./tasks/${file}`));
+readdirSync(path.resolve() + "/src/tasks").forEach(file => import(`./tasks/${file.substring(0, file.length - 3)}.js`));
 
 // Create and start the server :
 const schemas = await loadSchema(path.resolve() + "/resources/graphql/**/*.gql", {
