0fd16dd64fc659943d9926e2c142154be8ea1ca0
Bluzzi <camilledugas.pro@gmail.com>
2022-05-27 20:17:34 +0200

    ClientTester: add a test request

--- src/others/ClientTester.ts
+++ src/others/ClientTester.ts
@@ -1,16 +1,13 @@
 import { request, gql } from "graphql-request"; // https://www.npmjs.com/package/graphql-request
 
-const createMember = gql`
-
+const members = gql`
+    query Members {
+        members {
+            username
+        }
+    }
 `;
 
-const response = await request(
-    "http://localhost:3000",
-    createMember,
-    {},
-    {
-        authorization: "7fe47986-0ce9-4cfc-9294-cfc167ef32e8"
-    }
-);
-
-console.log(response);
+request("http://localhost:3006", members, {}, { authorization: "7fe47986-0ce9-4cfc-9294-cfc167ef32e8" }).then(response => {
+    console.log(response);
+});
