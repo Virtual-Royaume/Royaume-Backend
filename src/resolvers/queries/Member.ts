7a6d0acfbb6a2fa5d01bf79d34443192e2dd3623
Bluzzi <camilledugas.pro@gmail.com>
2022-06-24 16:51:42 +0200

    Review

--- src/resolvers/queries/Member.ts
+++ src/resolvers/queries/Member.ts
@@ -3,24 +3,26 @@ import memberCollection, { getMemberByDiscordId, getMembersWithPoints } from "..
 import { getFields } from "../../utils/GraphQL";
 
 const memberQuery: Resolvers["Query"] = {
-    members: async (_, __, ___, info) => {
+    members: async(_, __, ___, info) => {
         const fields = getFields(info);
 
         if (fields["activity.points.count"] || fields["activity.points.progress"]) {
             return await getMembersWithPoints();
         }
 
-        return await memberCollection.find({ isOnServer: true }).toArray() as Member[]; // 🤮
+        // Mandatory cast, field verification is done above, but not detected by TS :
+        return await memberCollection.find({ isOnServer: true }).toArray() as Member[];
     },
 
-    member: async (_, { id }, ___, info) => {
+    member: async(_, { id }, ___, info) => {
         const fields = getFields(info);
 
         if (fields["activity.points.count"] || fields["activity.points.progress"]) {
             return (await getMembersWithPoints()).find(member => member._id === id) ?? null;
         }
 
-        return await getMemberByDiscordId(id) as Member; // 🤮
+        // Mandatory cast, field verification is done above, but not detected by TS :
+        return await getMemberByDiscordId(id) as Member;
     }
 };
 
--- src/tasks/MonthUpdate.ts
+++ src/tasks/MonthUpdate.ts
@@ -2,7 +2,7 @@ import memberCollection, { getMembersWithPoints } from "../database/collections/
 import tier from "../../resources/config/tier.json";
 import { TierUpdate } from "../interfaces/ServerSchema";
 
-setInterval(async () => {
+setInterval(async() => {
     const date = new Date();
 
     if (date.getDate() === 1 && date.getHours() === 0 && date.getMinutes() === 0) {
@@ -11,12 +11,18 @@ setInterval(async () => {
 
         for (const member of members) {
             if (member.activity.points.progress === TierUpdate.Up) {
-                if (member.activity.tier !== tier.max) await memberCollection.updateOne({ _id: member._id }, { $inc: { "activity.tier": -1 } });
+                if (member.activity.tier !== tier.max) {
+                    await memberCollection.updateOne({ _id: member._id }, { $inc: { "activity.tier": -1 } });
+                }
+
                 continue;
             }
 
             if (member.activity.points.progress === TierUpdate.Down) {
-                if (member.activity.tier !== tier.min) await memberCollection.updateOne({ _id: member._id }, { $inc: { "activity.tier": 1 } });
+                if (member.activity.tier !== tier.min) {
+                    await memberCollection.updateOne({ _id: member._id }, { $inc: { "activity.tier": 1 } });
+                }
+
                 continue;
             }
         }
