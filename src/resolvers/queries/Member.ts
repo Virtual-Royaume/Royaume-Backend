2506918102c1890ca1da2e5f1242a6f8b8a1ba6e
Bluzzi <camilledugas.pro@gmail.com>
2022-07-07 16:22:22 +0200

    Finish implementation of activity points

--- src/resolvers/queries/Member.ts
+++ src/resolvers/queries/Member.ts
@@ -2,27 +2,53 @@ import { Member, Resolvers } from "../../interfaces/ServerSchema";
 import memberCollection, { getMemberByDiscordId, getMembersWithPoints } from "../../database/collections/Member";
 import { getFields } from "../../utils/GraphQL";
 
+/**
+ * TODO V4
+ *
+ * This file contains some casts due to the operation of GraphQL which would add a substantial overhead only to fix some types.
+ *
+ * This will be corrected in a future major version of the project as the point system will be based on the DB for optimization reasons.
+*/
+
 const memberQuery: Resolvers["Query"] = {
     members: async(_, __, ___, info) => {
         const fields = getFields(info);
 
         if (fields["activity.points.count"] || fields["activity.points.progress"]) {
-            return await getMembersWithPoints();
+            const members = await memberCollection.find({ isOnServer: true }).toArray();
+            const membersWithPoints = await getMembersWithPoints();
+
+            for (const member of members) {
+                const points = membersWithPoints.find(m => m._id === member._id)?.points;
+
+                if (!points) continue;
+
+                (member as Member).activity.points = points;
+            }
+
+            return members as Member[];
         }
 
-        // Mandatory cast, field verification is done above, but not detected by TS :
         return await memberCollection.find({ isOnServer: true }).toArray() as Member[];
     },
 
     member: async(_, { id }, ___, info) => {
+        const member = await getMemberByDiscordId(id) as Member;
         const fields = getFields(info);
 
         if (fields["activity.points.count"] || fields["activity.points.progress"]) {
-            return (await getMembersWithPoints()).find(member => member._id === id) ?? null;
+            const points = (await getMembersWithPoints()).find(m => m._id === id)?.points;
+
+            if (points) {
+                member.activity.points = points;
+
+                return member;
+            }
+
+            return member;
         }
 
-        // Mandatory cast, field verification is done above, but not detected by TS :
-        return await getMemberByDiscordId(id) as Member;
+        return member;
     }
 };
 
