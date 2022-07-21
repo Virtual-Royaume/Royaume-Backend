a21725851bdf1bddb6ccc82f929e6c899ca10482
Bluzzi <camilledugas.pro@gmail.com>
2022-07-22 00:54:49 +0200

    Scalar (revolvers): adapting for DayJS

--- src/resolvers/Scalar.ts
+++ src/resolvers/Scalar.ts
@@ -1,20 +1,21 @@
 import { GraphQLScalarType, Kind } from "graphql";
 import { Resolvers } from "../interfaces/ServerSchema";
 import { getDateWithoutTime } from "../utils/Date";
+import DayJS from "../utils/DayJS";
 
 export const date: Resolvers["Date"] = new GraphQLScalarType({
     name: "Date",
     description: "Date without time",
 
     serialize(value) {
-        return getDateWithoutTime(value as Date).getTime();
+        return getDateWithoutTime(DayJS(value as Date)).millisecond();
     },
     parseValue(value) {
-        return getDateWithoutTime(new Date(value as string));
+        return getDateWithoutTime(DayJS(value as string));
     },
     parseLiteral(ast) {
         return ast.kind === Kind.STRING
-            ? getDateWithoutTime(new Date(ast.value))
+            ? getDateWithoutTime(DayJS(ast.value))
             : null;
     }
 });
