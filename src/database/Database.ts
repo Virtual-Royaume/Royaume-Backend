30e4304b3fd5dcba57a3058125a60f81282d8e72
Bluzzi <camilledugas.pro@gmail.com>
2022-08-03 14:11:19 +0200

    Database: use env variable instead of MongoDB config

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,36 +1,9 @@
-import { existsSync, readFileSync } from "fs";
 import { MongoClient } from "mongodb";
 
-// Check if connection informations exist :
-if (!existsSync(`${__dirname}/../../resources/auth/mongodb.json`)) {
-    throw new Error(
-        "You need to make a copy of _mongodb.json without the underscore and fill in the missing elements."
-    );
-}
+if (!process.env.MONGO_URL) throw new Error("The environment variable MONGO_URL is missing");
 
-// Get connection information :
-interface ConnectInfo {
-    host: string;
-    port: number;
-
-    username: string;
-    password: string;
-
-    database: string;
-}
-
-const connectInfo: ConnectInfo = JSON.parse(
-    readFileSync(`${__dirname}/../../resources/auth/mongodb.json`, "utf-8")
-);
-
-// Connection to the dababase :
-const connectLink = {
-    normal: `mongodb://${connectInfo.username}:${connectInfo.password}@${connectInfo.host}:${connectInfo.port}/${connectInfo.database}`,
-    local: `mongodb://${connectInfo.host}:${connectInfo.port}/${connectInfo.database}`
-};
-
-const client = new MongoClient(connectLink.normal);
+const client = new MongoClient(process.env.MONGO_URL);
 
 client.connect();
 
-export default client.db(connectInfo.database);
+export default client.db("royaume");
