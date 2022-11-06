921615c9576626e074d3c78e630f8b99f00eff23
Bluzzi <camilledugas.pro@gmail.com>
2022-11-06 02:18:28 +0100

    main: typing

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { getNumberEnv } from "$utils/env-variable";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-async function main() {
+async function main(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   await app.listen(getNumberEnv("PORT"));
