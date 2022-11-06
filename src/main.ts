bc40ed05ac020924f132702f65459398d62326ce
Bluzzi <camilledugas.pro@gmail.com>
2022-11-06 01:16:29 +0100

    main: remove tests

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import { getNumberEnv } from "$utils/env-variable";
-import { isDevEnvironment, isProdEnvironment } from "$utils/environment";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function main() {
   const app = await NestFactory.create(AppModule);
-
-  console.log(isDevEnvironment, isProdEnvironment);
-  console.log(process.argv);
 
   await app.listen(getNumberEnv("PORT"));
 }
