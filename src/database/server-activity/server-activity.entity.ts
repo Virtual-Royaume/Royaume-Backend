643821409e5acaaad8d425cac41597c92d6b75b9
Bluzzi <camilledugas.pro@gmail.com>
2022-11-07 18:54:44 +0100

    server-activity (database/entity): fix default date

--- src/database/server-activity/server-activity.entity.ts
+++ src/database/server-activity/server-activity.entity.ts
@@ -4,7 +4,7 @@ import { ModelOptions, Prop } from "@typegoose/typegoose";
 @ModelOptions({ schemaOptions: { collection: "serveractivity" } })
 export class ServerActivity {
 
-  @Prop({ index: true, unique: true, default: dayJS().utc().toDate() })
+  @Prop({ index: true, unique: true, default: dayJS().utc().startOf("day").toDate() })
   public date!: Date;
 
   @Prop({ default: 0 })
