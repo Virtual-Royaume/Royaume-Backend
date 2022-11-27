143c2ca74bb65288c2e908ad70c4ead2158514e2
Bluzzi <camilledugas.pro@gmail.com>
2022-11-27 07:27:22 +0100

    Create all Member database entities

--- src/database/member/member.entity.ts
+++ src/database/member/member.entity.ts
@@ -1,21 +1,27 @@
-import { ModelOptions, Prop } from "@typegoose/typegoose";
-import { DiscordActivity } from "./sub-documents/discord-activity";
+import { tierConfig } from "$config/tier";
+import { ModelOptions, Prop, Ref } from "@typegoose/typegoose";
+import { MemberChannelActivity } from "./member-channel-activity.entity";
+import { MemberInformation } from "./member-information.entity";
+import { MemberLinks } from "./member-links.entity";
 
 @ModelOptions({ schemaOptions: { collection: "member" } })
 export class Member {
 
-  @Prop({ required: true })
-  public username!: string;
-
-  @Prop({ required: true })
-  public profilePicture!: string;
-
-  @Prop()
-  public birthday?: Date;
+  @Prop({ required: true, index: true, unique: true })
+  public _id!: string;
 
   @Prop({ default: true })
   public isOnServer!: boolean;
 
-  @Prop({ _id: false })
-  public activity!: DiscordActivity;
+  @Prop({ default: tierConfig.min })
+  public activityTier!: number;
+
+  @Prop({ ref: () => MemberInformation, required: true })
+  public information!: Ref<MemberInformation>;
+
+  @Prop({ ref: () => MemberLinks, required: true })
+  public links!: Ref<MemberLinks>;
+
+  @Prop({ ref: () => MemberChannelActivity, default: [] })
+  public channelActivity!: Ref<MemberChannelActivity>[];
 }
