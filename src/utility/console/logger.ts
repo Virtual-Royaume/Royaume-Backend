682c30f3de65ecf40b0fad5c6904ea42cb9c6b52
Bluzzi <camilledugas.pro@gmail.com>
2022-11-27 13:23:01 +0100

    logger (utility): improve log design

--- src/utility/console/logger.ts
+++ src/utility/console/logger.ts
@@ -1,36 +1,39 @@
+import { dayJS } from "$utility/day-js";
 import { LoggerService } from "@nestjs/common";
 import { ConsoleEffect, ConsoleForground } from "./console-style";
 
 export class Logger implements LoggerService {
 
-  private formatLog(iconColor: ConsoleForground, icon: string, type: string, message: string): string {
+  private formatLog(color: ConsoleForground, type: string, message: string): string {
     const maxSpace = 8;
     const spaceSize = maxSpace - type.length;
 
-    const prefix = `${iconColor}${icon} ${ConsoleEffect.Bold}${ConsoleForground.White}${type}`;
-    const separator = `${ConsoleEffect.Reset}${" ".repeat(spaceSize)}${ConsoleForground.Gray}» `;
+    const datetime = dayJS().format("YYYY-MM-DD HH:mm:ss");
+
+    const prefix = `${ConsoleForground.White}[${datetime}] ${ConsoleEffect.Bold}${color}${type.toUpperCase()}`;
+    const separator = `${ConsoleEffect.Reset} ${ConsoleForground.Gray}${"-".repeat(spaceSize)}» `;
     const content = `${ConsoleEffect.Reset}${message}`;
 
     return `${prefix}${separator}${content}`;
   }
 
   public log(message: string): void {
-    console.log(this.formatLog(ConsoleForground.Blue, "-", "Info", message));
+    console.log(this.formatLog(ConsoleForground.Blue, "info", message));
   }
 
   public error(message: string): void {
-    console.log(this.formatLog(ConsoleForground.Red, "x", "Error", message));
+    console.log(this.formatLog(ConsoleForground.Red, "error", message));
   }
 
   public warn(message: string): void {
-    console.log(this.formatLog(ConsoleForground.Yellow, "!", "Warn", message));
+    console.log(this.formatLog(ConsoleForground.Yellow, "warn", message));
   }
 
   public debug(message: string): void {
-    console.log(this.formatLog(ConsoleForground.Cyan, "o", "Debug", message));
+    console.log(this.formatLog(ConsoleForground.Cyan, "debug", message));
   }
 
   public verbose(message: string): void {
-    console.log(this.formatLog(ConsoleForground.Magenta, "~", "Verbose", message));
+    console.log(this.formatLog(ConsoleForground.Magenta, "verbose", message));
   }
 }
