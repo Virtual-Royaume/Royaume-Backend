492a52a80f64b67b1f6c9645e0f559dc1606c526
Bluzzi <camilledugas.pro@gmail.com>
2022-11-27 07:27:22 +0100

    Create all Member database entities

--- src/database/member/member.service.ts
+++ src/database/member/member.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from "@nestjs/common";
 import { ReturnModelType } from "@typegoose/typegoose";
 import { InjectModel } from "@m8a/nestjs-typegoose";
 import { Member } from "./member.entity";
+import { MemberChannelActivity } from "./member-channel-activity.entity";
+import { MemberInformation } from "./member-information.entity";
+import { MemberLinks } from "./member-links.entity";
 
 @Injectable()
 export class MemberService {
 
   constructor(
-    @InjectModel(Member) private readonly memberModel: ReturnModelType<typeof Member>
+    @InjectModel(Member) private readonly memberModel: ReturnModelType<typeof Member>,
+    @InjectModel(MemberChannelActivity) private readonly memberChannelActivity: ReturnModelType<typeof MemberChannelActivity>,
+    @InjectModel(MemberInformation) private readonly memberInformation: ReturnModelType<typeof MemberInformation>,
+    @InjectModel(MemberLinks) private readonly memberLinks: ReturnModelType<typeof MemberLinks>
   ) {}
 
   public async findAll(isOnServer = true): Promise<Member[]> {
