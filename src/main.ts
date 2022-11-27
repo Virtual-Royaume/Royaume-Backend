da14e1103bd210d1fef8c1774a3c1b06eea79ddc
Bluzzi <camilledugas.pro@gmail.com>
2022-11-27 13:00:24 +0100

    main: use our custom logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { environmentVariable } from "$config/environment-variable";
+import { Logger } from "$utility/console/logger";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function main(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: new Logger() });
 
   await app.listen(environmentVariable().port);
 }
