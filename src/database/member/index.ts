73aa66f5ddc808a92ea1e1ec41fcf7804ed90530
Bluzzi <camilledugas.pro@gmail.com>
2023-03-09 09:59:37 +0100

    revert old backend

--- src/database/member/index.ts
+++
@@ -1,3 +0,0 @@
-export { MemberModule } from "./member.module";
-export { MemberService } from "./member.service";
-export { Member } from "./member.entity";
--- src/database/server-activity/index.ts
+++
@@ -1,3 +0,0 @@
-export { ServerActivityModule } from "./server-activity.module";
-export { ServerActivityService } from "./server-activity.service";
-export { ServerActivity } from "./server-activity.entity";
