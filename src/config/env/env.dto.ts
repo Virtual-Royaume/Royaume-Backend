d71a0d038af44b9b777b0f7e1679f676d6cae416
Bluzzi <camilledugas.pro@gmail.com>
2023-06-04 02:57:06 +0200

    fix(env.dto.ts): change POSTGRES_URI to POSTGRES_URL to match the actual environment variable name

--- src/config/env/env.dto.ts
+++ src/config/env/env.dto.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 
 export const envDTO = z.object({
   PORT: z.number(),
-  POSTGRES_URI: z.string().url(),
+  POSTGRES_URL: z.string().url(),
   API_TOKEN: z.string().uuid()
 });
