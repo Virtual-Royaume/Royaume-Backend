76a9a5d6cbd75780f3212eeb48d43c57b1948463
Bluzzi <camilledugas.pro@gmail.com>
2023-06-04 21:53:15 +0200

    feat(app.module.ts): add ApolloServerPluginLandingPageLocalDefault plugin to GraphQLModule plugins array to enable landing page for GraphQL API refactor(app.module.ts): set playground option to false in GraphQLModule.forRoot to disable GraphQL playground in production environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { ApolloDriver } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { APIModule } from "#/api/api.module";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 
 @Module({
   imports: [
     // GraphQL setup:
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true
+      autoSchemaFile: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()]
     }),
 
     // API module:
